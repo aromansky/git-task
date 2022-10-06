94a1189aba49b97ec20fa0099f80b6f59f529dc9
Alexey Romanskii <kirbi.zsthan@gmail.com>
2022-10-06 11:36:11 +0300

    Add sum days between year_1 and year_2

--- DateTime.pas
+++ DateTime.pas
@@ -36,14 +36,14 @@ begin
   Assert(year > 0, 'Год не может быть отрицательным!');
   PrintLn($'Дней в году: {if year.Divs(4) and (year.Divs(400)or not year.Divs(100)) then 366 else 365}');
   
-  //var (year_1, year_2) := ReadInteger2($'{newline}Введите года: ');
-  //Assert((year_1 > 0) and (year_2 > 0), 'Год не может быть отрицательным!');
-  //var sum := 0;
-  //if year_1 > year_2 then
-  //  Swap(year_1, year_2);
-  //for var i := year_1 to year_2 do
-  //  sum += if i.Divs(4) and (i.Divs(400) or not i.Divs(100)) then 366 else 365;
-  //PrintLn($'Дней в этих годах: {sum}');
+  var (year_1, year_2) := ReadInteger2($'{newline}Введите года: ');
+  Assert((year_1 > 0) and (year_2 > 0), 'Год не может быть отрицательным!');
+  var sum := 0;
+  if year_1 > year_2 then
+    Swap(year_1, year_2);
+  for var i := year_1 to year_2 do
+    sum += if i.Divs(4) and (i.Divs(400) or not i.Divs(100)) then 366 else 365;
+  PrintLn($'Дней в этих годах: {sum}');
   
   //var hours := ReadInteger($'{newline}Введите кол-во часов:');
   //Assert(hours >= 0, 'Кол-во часов не может быть отрицательным!');
